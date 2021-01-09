9ebddfa98073264ae3d66371462624a15a3cadd3
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-09 19:27:15 +0800

    adjusted codes for Roll and Hold Button

--- app.js
+++ app.js
@@ -10,20 +10,85 @@ GAME RULES:
 */
 
 
-var globalScore;
-var currentScore;
-let dicePNG = document.querySelector("body > div > img");
+/**********************************************
+*** VARIABLES
+**********************************************/
+
+
+let globalScore = 0;
+let roundScore = 0; 
+let currentPlayer = 0;
+
+let dicePNG = document.querySelector("body > div > img.dice");
+
 let playerOneScore = document.querySelector("#score-0");
 let playerTwoScore = document.querySelector("#score-1");
+
 let playerOneRoundScore = document.querySelector("#current-0");
 let playerTwoRoundScore = document.querySelector("#current-1");
 
-//New Game
+let diceObject = document.querySelector(".dice");
+
+let rollBtn = document.querySelector("body > div > button.btn-roll");
+let holdBtn = document.querySelector("body > div > button.btn-hold");
+
+let player1Panel = document.querySelector("body > div > div.player-0-panel");
+let player2Panel = document.querySelector("body > div > div.player-1-panel");
+
+let modal = document.getElementById("popup1");
+
+let winnerName = document.getElementById("winner player-name");
+
+
+
+
+/**********************************************
+*** APP
+**********************************************/
+
+
+//Initialize New Game
 document.querySelector('.btn-new').addEventListener('click', newGame);
 
+//Roll Dice Button
+rollBtn.addEventListener(
+    'click',
+    function() {
+        let DiceArr = [1, 2, 3, 4, 5, 6];
+        let randomDice = DiceArr[Math.floor(Math.random() * DiceArr.length)];
+
+        diceObject.src = "dice-" + randomDice + ".png";
+        if (randomDice !== 1) {
+            roundScore += randomDice;
+            document.querySelector('#current-' + currentPlayer).innerHTML = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }
+
+);
+
+//Hold Score Button
+holdBtn.addEventListener(
+    'click',
+    function() {
+        globalScore[currentPlayer] += roundScore;
+        document.querySelector('#score-' + currentPlayer).innerHTML = globalScore[currentPlayer];
+        checkWinner();
+    }
+);
+
+
+
+/**********************************************
+*** FUNCTIONS
+**********************************************/
+
+
 function newGame() {
-    globalScore = 0;
-    currentScore = 0;
+    globalScore = [0, 0];
+    roundScore = 0;
+    currentPlayer = 0;
 
     dicePNG.src = "dice-1.png";
 
@@ -34,7 +99,44 @@ function newGame() {
     playerTwoRoundScore.innerHTML = "0";
 }
 
-//Hold Button
-document.querySelector('.btn-hold').addEventListener('click', function() {
-    
-})
+function nextPlayer() {
+    if (currentPlayer === 0) {
+        currentPlayer = 1;
+    }
+    else {
+        currentPlayer = 0;
+    }
+
+    roundScore = 0;
+    playerOneRoundScore.innerHTML = "0";
+    playerTwoRoundScore.innerHTML = "0";
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+
+
+function checkWinner() {
+    let playerName = document.querySelector('.player-name').innerHTML;
+    if (globalScore[currentPlayer] >= 100) {
+        congratulations(playerName);
+    } else {
+        nextPlayer();
+    }
+}
+
+function congratulations(playerName) {
+    winnerName.textContent = playerName;
+    // show congratulations modal
+    modal.classList.add("show");
+
+    //closeicon on modal
+    closeModal();
+}
+
+// @desciption for user to play Again 
+function playAgain() {
+    modal.classList.remove("show");
+    newGame();
+}
+
